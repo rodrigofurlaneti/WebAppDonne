2210d967d858fdbc01a23ebb9f417be955a65032
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2023-05-13 11:21:45 -0300

    Ajuste

---
+++ WebAppDonne/Controllers/BuyerCommandController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppDonne.Dal;
+using WebAppDonne.Models;
+
+namespace WebAppDonne.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BuyerCommandController : Controller
+    {
+        private readonly ILogger<BuyerController> _logger;
+
+        public BuyerCommandController(ILogger<BuyerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetBuyerCommand")]
+        public IEnumerable<BuyerModel> Get()
+        {
+            BuyerCommandRepository dal = new BuyerCommandRepository();
+            var ret = dal.GetBuyerCommand();
+            return (ret);
+        }
+    }
+}
---
+++ WebAppDonne/Dal/BuyerCommandRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using WebAppDonne.Models;
+
+namespace WebAppDonne.Dal
+{
+    public class BuyerCommandRepository
+    {
+        #region Properties
+
+        private readonly IConfigurationRoot configurationRoot;
+
+        #endregion
+
+        #region Constructor
+        public BuyerCommandRepository()
+        {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            configurationRoot = configurationBuilder.Build();
+        }
+        #endregion
+
+        #region Methods 
+
+        public IEnumerable<BuyerModel> GetBuyerCommand()
+        {
+            string ConnectionString = configurationRoot.GetConnectionString("localHost");
+            List<BuyerModel> listBuyerModel = new List<BuyerModel>();
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("USP_BuyerCommandGetByStatus", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    BuyerModel buyerModel = new BuyerModel();
+                    buyerModel.BuyerId = Convert.ToInt32(rdr["BuyerId"]);
+                    buyerModel.BuyerName = Convert.ToString(rdr["BuyerName"]);
+                    buyerModel.BuyerPhone = Convert.ToString(rdr["BuyerPhone"]);
+                    buyerModel.BuyerAddress = Convert.ToString(rdr["BuyerAddress"]);
+                    buyerModel.DateInsert = Convert.ToDateTime(rdr["DateInsert"]);
+                    buyerModel.DateUpdate = Convert.ToDateTime(rdr["DateUpdate"]);
+                    buyerModel.UserId = Convert.ToInt32(rdr["UserId"]);
+                    buyerModel.UserName = Convert.ToString(rdr["UserName"]);
+                    listBuyerModel.Add(buyerModel);
+                }
+            }
+            return listBuyerModel;
+        }
+
+        #endregion
+    }
+}
