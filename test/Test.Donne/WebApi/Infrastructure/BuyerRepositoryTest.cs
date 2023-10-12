96ac2c263c9e781b06cea60cdb5b2a7c54ec6252
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-12 11:21:15 -0300

    Add

---
+++ test/Test.Donne/WebApi/Infrastructure/BuyerRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi.Donne.Infrastructure;
+
+namespace Test.Donne
+{
+    [TestClass]
+    public class BuyerRepositoryTest
+    {
+        [TestMethod]
+        public void GetAllBuyers_Sucesso()
+        {
+            // Arrange
+            BuyerRepository buyerRepository = new BuyerRepository();
+            // Act
+            var result = buyerRepository.GetAllBuyers();
+
+            // Assert
+            Assert.IsNotNull(buyerRepository);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count() > 0);
+        }
+    }
+}
