75860f353fe77c9b09b2d149e7ef3c07ae123a6e
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-17 10:26:34 -0300

    Add teste

---
+++ test/Test.Donne/WebApi/Infrastructure/BuyerRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Donne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi.Donne.Infrastructure;
+
+namespace Test.Donne.WebApi.Infrastructure.BuyerRepositoryTest
+{
+    [TestClass]
+    [TestCategory("Donne > WebApi > Infrastructure > BuyerRepository")]
+    public class BuyerRepositoryTest
+    {
+        [TestMethod]
+        public void GetAllBuyers_Sucesso()
+        {
+            // Arrange
+            BuyerRepository buyerRepository = new BuyerRepository();
+
+            // Act
+            var result = buyerRepository.GetAllBuyers();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
