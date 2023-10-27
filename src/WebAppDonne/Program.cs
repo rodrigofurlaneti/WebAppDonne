de00776ef40bcb04bec5ca57fa7dc642f0c203bb
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-27 12:34:31 -0300

    Add teste

--- src/WebAppDonne/Program.cs
+++ src/WebAppDonne/Program.cs
@@ -1,13 +1,4 @@
-var AllowPolicy = "_AllowPolicy";
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: AllowPolicy, builder => builder.WithOrigins("https://apidonne.azurewebsites.net/")
-                                                        .AllowAnyMethod()
-    .AllowAnyHeader());
-});
 
 builder.Services.AddScoped<WebApi.Donne.Infrastructure.SeedWork.ILogger, WebApi.Donne.Infrastructure.SeedWork.Logger>();
 
@@ -27,7 +18,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 
 app.UseHttpsRedirection();
 
-app.UseCors(AllowPolicy);
+app.UseCors();
 
 app.UseAuthorization();
 
