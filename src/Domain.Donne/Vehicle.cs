6a40acbafaa47dd5dc913b0b1b6171ca4522207a
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2024-02-08 17:34:42 -0300

    Ajuste

--- src/Domain.Donne/Vehicle.cs
+++ src/Domain.Donne/Vehicle.cs
@@ -5,17 +5,17 @@
         public int VehicleId { get; set; }
         public int VehicleTypeId { get; set; }
         public string VehicleTypeName { get; set; }
-        public int BrandId { get; set; }
-        public string BrandName { get; set; }
-        public int ModelId { get; set; }
-        public string ModelName { get; set; }
-        public int ColorId { get; set; }
-        public string ColorName { get; set; }
+        public int VehicleBrandId { get; set; }
+        public string VehicleBrandName { get; set; }
+        public int VehicleModelId { get; set; }
+        public string VehicleModelName { get; set; }
+        public int VehicleColorId { get; set; }
+        public string VehicleColorName { get; set; }
         public string Plate { get; set; }
         public string EntryDate { get; set; }
         public string EntryTime { get; set; }
         public string DepartureDate { get; set; }
         public string DepartureTime { get; set; }
-        public bool Parked { get; set; }
+        public int Parked { get; set; }
     }
 }
